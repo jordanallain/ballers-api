afd6907c956f14327acd0f4e611e4a1e26d186fb
jordanallain <jallain@gmail.com>
2016-08-21 18:18:31 -0400

    Scaffold Favorites join table
    Genereate scaffold for Favorites join table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821221404) do
+ActiveRecord::Schema.define(version: 20160821221732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20160821221404) do
   end
 
   add_index "examples", ["user_id"], name: "index_examples_on_user_id", using: :btree
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "player_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "favorites", ["player_id"], name: "index_favorites_on_player_id", using: :btree
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
 
   create_table "players", force: :cascade do |t|
     t.text     "given_name"
@@ -51,4 +61,6 @@ ActiveRecord::Schema.define(version: 20160821221404) do
   add_index "users", ["token"], name: "index_users_on_token", unique: true, using: :btree
 
   add_foreign_key "examples", "users"
+  add_foreign_key "favorites", "players"
+  add_foreign_key "favorites", "users"
 end
