2c8dce0ca1f13ab880522afb6cabf70eebba2ba7
jordanallain <jallain@gmail.com>
2016-08-22 14:58:55 -0400

    Add unique validation to favorite model
    Only allow a user to add a player to his favorites list one time.

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,6 @@
 class Favorite < ActiveRecord::Base
   belongs_to :user, inverse_of: :favorites
   belongs_to :player, inverse_of: :favorites
+
+  validates_uniqueness_of :user_id, scope: :player_id
 end
