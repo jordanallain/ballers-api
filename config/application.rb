a94140c77a4723d26a6d53a1d2908dc3dd822ef1
jordanallain <jallain@gmail.com>
2016-08-22 23:55:56 -0400

    Remove secrets
    Remove secrets.

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module BallersApi
     # auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names.
     # Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Eastern Time (US & Canada)'
 
     # The default locale is :en and all translations from
     # config/locales/*.rb,yml are auto loaded.
