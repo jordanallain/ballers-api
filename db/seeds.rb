9c5a72fd3c8b97df46d382f64bd823dcd0ce7494
jordanallain <jallain@gmail.com>
2016-08-22 20:47:10 -0400

    Add seed data to back end
    Add seed data to send to Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,19 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
+Player.create(given_name: 'Isaiah', surname: 'Thomas', team: 'Celtics', position: 'PG', points_per_game: 22, rebounds_per_game: 3, assists_per_game: 6, twitter: 'https://twitter.com/isaiah_thomas', instagram: 'https://www.instagram.com/isaiahthomas/')
+
+Player.create(given_name: 'Jeremy', surname: 'Lin', team: 'Nets', position: 'PG', points_per_game: 12, rebounds_per_game: 3, assists_per_game: 3, twitter: 'https://twitter.com/jlin7', instagram: 'https://www.instagram.com/jlin7/')
+
+Player.create(given_name: 'Carmelo', surname: 'Anthony', team: 'Knicks', position: 'SF', points_per_game: 22, rebounds_per_game: 4, assists_per_game: 8, twitter: 'https://twitter.com/carmeloanthony', instagram: 'https://www.instagram.com/carmeloanthony/')
+
+Player.create(given_name: 'Jahlil', surname: 'Okafor', team: '76ers', position: 'C', points_per_game: 18, rebounds_per_game: 7, assists_per_game: 1, twitter: 'https://twitter.com/jahlilokafor', instagram: 'https://www.instagram.com/jah8/')
+
+Player.create(given_name: 'Kyle', surname: 'Lowry', team: 'Raptor', position: 'PG', points_per_game: 21, rebounds_per_game: 5, assists_per_game: 6, twitter: 'https://twitter.com/klow7', instagram: 'https://www.instagram.com/kyle_lowry7/')
+
+Player.create(given_name: 'Jimmy', surname: 'Butler', team: 'Bulls', position: 'SF', points_per_game: 21, rebounds_per_game: 5, assists_per_game: 5, twitter: 'https://twitter.com/jimmybutler', instagram: 'https://www.instagram.com/jimmybutler/')
+
+Player.create(given_name: 'Kyrie', surname: 'Irving', team: 'Cavaliers', position: 'PG', points_per_game: 20, rebounds_per_game: 3, assists_per_game: 6, twitter: 'https://twitter.com/kyrieirving', instagram: 'https://www.instagram.com/kyrieirving/')
+
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
